fda720185541b45f28f3888722c49da8a3af115b
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2024-04-16 16:53:37 +0000

    devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     my_page_path
@@ -9,6 +10,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :telephone_number, :postal_code, :address])
+  end
+  
+end
 
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
