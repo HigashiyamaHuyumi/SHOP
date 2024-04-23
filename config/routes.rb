ff4f7f828e8cc959ea6c1ed2c957706c77686a85
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2024-04-23 14:47:07 +0000

    install swiper

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
 
 
   scope module: 'users' do
-		get '/users/mypage', to: 'users#mypage', as: 'mypage'
-		get '/users/mypage/info', to: 'users#info', as: 'info'
-		get '/users/mypage/info_edit', to: 'users#edit', as: 'edit'
-    resources :users, only: [:show, :update] do
-		  get :confirm, on: :member # 退会確認
-      patch :withdrawal, on: :member # 退会処理
-		end
+ 		get '/users/mypage', to: 'users#mypage', as: 'mypage'
+ 		get '/users/mypage/info', to: 'users#info', as: 'info'
+ 		get '/users/mypage/info_edit', to: 'users#edit', as: 'edit'
+     resources :users, only: [:show, :update] do
+ 		  get :confirm, on: :member # 退会確認
+       patch :withdrawal, on: :member # 退会処理
+ 		end
   end
 
   root to: "homes#top"
